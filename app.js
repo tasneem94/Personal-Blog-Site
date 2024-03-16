fa11111e3e8fb34952422371cf23dd184c0396a8
tasneem94 <tasneemsakif31@gmail.com>
2024-03-16 16:18:25 +0600

    Adding Blogs

--- app.js
+++ app.js
@@ -1,17 +1,64 @@
 // import express from "express";
 const express = require("express");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 //express app
 const app = express();
 
+const dbURI =
+  "mongodb+srv://tasneemsakif31:blog310397@cluster-blog.7dodqmo.mongodb.net/blogs-database?retryWrites=true&w=majority&appName=Cluster-blog";
+
+mongoose
+  .connect(dbURI)
+  .then((res) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 //register view engine
 app.set("view engine", "ejs");
 
-//listen for request
-app.listen(3000);
-
 //middleware static files
 app.use(express.static("public"));
+
+//mongoose and mongo sandbox routes
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "New Blog 2",
+    snippet: "about my new blog",
+    body: "Deatils abot my new blog",
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      comsole.log(err);
+    });
+});
+
+app.get("/single-blog", (req, res) => {
+  Blog.findById("65f57095f838f205cf10a309")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      comsole.log(err);
+    });
+});
+
+//
 
 app.get("/", (req, res) => {
   const blogs = [

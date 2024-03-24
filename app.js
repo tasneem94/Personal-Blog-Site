e319ca5c2db48eeae22e9c6d0c65707375c41abf
tasneem94 <tasneemsakif31@gmail.com>
2024-03-24 16:52:43 +0600

    added routes and controllers

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // import express from "express";
 const express = require("express");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+
 const { render } = require("ejs");
+const blogRoutes = require("./routes/blogRoutes");
 
 //express app
 const app = express();
@@ -37,56 +38,10 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
-//blog routes
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { title: "All blogs", blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+//nlog routes
+app.use("/blogs", blogRoutes);
 
-app.post("/blogs", (req, res) => {
-  const blog = new Blog(req.body);
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create a New Blog" });
-});
-
-app.get("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then((result) => {
-      res.render("details", { title: "Blog details", blog: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.delete("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: "/blogs" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
+//404 page
 app.use((req, res) => {
   res.status(404).render("error", { title: "404" });
 });
